9c197031ab3f9f3fd115faf6108e72ef57c74d7c
1vav <akshayvaidya@gmail.com>
2024-07-29 17:39:42 +0300

    Update build.sh

--- packages/getTextFromDoc/getTextFromDoc/build.sh
+++ packages/getTextFromDoc/getTextFromDoc/build.sh
@@ -3,4 +3,4 @@
 set -e
 
 virtualenv -p /opt/homebrew/Cellar/python@3.11/3.11.4/bin/python3.11 virtualenv
-python3.11 -m pip install -r requirements.txt --platform manylinux2010_x86_64 --only-binary=:all: --upgrade --target virtualenv/lib/python3.11/site-packages
+python3.11 -m pip install -r requirements.txt --platform manylinux2014_x86_64 --only-binary=:all: --upgrade --target virtualenv/lib/python3.11/site-packages
