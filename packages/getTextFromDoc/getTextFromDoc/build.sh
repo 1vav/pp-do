60d7280179ee6d7a8e7406a9abab4d33f0cfb269
1vav <akshayvaidya@gmail.com>
2024-07-29 17:29:27 +0300

    pillow

--- packages/getTextFromDoc/getTextFromDoc/build.sh
+++ packages/getTextFromDoc/getTextFromDoc/build.sh
@@ -3,6 +3,4 @@
 set -e
 
 virtualenv -p /opt/homebrew/Cellar/python@3.11/3.11.4/bin/python3.11 virtualenv
-source virtualenv/bin/activate
-python3.11 -m pip install --no-deps --implementation cp --only-binary=:all: --platform manylinux2010_x86_64 -r requirements.txt --target virtualenv/bin
-deactivate
+python3.11 -m pip install -r requirements.txt --platform manylinux2010_x86_64 --only-binary=:all: --upgrade --target virtualenv/lib/python3.11/site-packages
